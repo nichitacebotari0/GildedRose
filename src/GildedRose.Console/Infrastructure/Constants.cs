8b5e18ede6227bac63887f50fe14278d4ac15776
Nichita Cebotari <Nichita.Cebotari@endava.com>
2018-10-28 14:53:46 +0200

    Refactored Console.

---
+++ src/GildedRose.Console/Infrastructure/Constants.cs
@@ -0,0 +1,15 @@
+﻿namespace GildedRose.Console
+{
+    public static class Constants
+    {
+        public const int MinQuality = 0;
+        public const int MaxQuality = 50;
+
+        public const string AgedBrie = "Aged Brie";
+        public const string DexVestPlus5 = "+5 Dexterity Vest";
+        public const string MongooseElixir = "Elixir of the Mongoose";
+        public const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+        public const string BackstagePass = "Backstage passes to a TAFKAL80ETC concert";
+        public const string ConjuredManaCake = "Conjured Mana Cake";
+    }
+}
---
+++ src/GildedRose.Console/ItemQualityManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+
+namespace GildedRose.Console
+{
+    static class ItemQualityManager
+    {
+
+        public static void UpdateQuality(IList<Item> items)
+        {
+            foreach (var item in items)
+            {
+                var sellInVelocity = GetSellInVelocity(item.Name);
+                item.SellIn += sellInVelocity;
+
+                var qualityVelocity = GetQualityVelocity(item);
+                if (item.Quality > Constants.MinQuality && item.Quality < Constants.MaxQuality)
+                {
+                    item.Quality += qualityVelocity;
+                    if (item.Quality > Constants.MaxQuality)
+                    {
+                        item.Quality = Constants.MaxQuality;
+                    }
+                    if (item.Quality < Constants.MinQuality)
+                    {
+                        item.Quality = Constants.MinQuality;
+                    }
+                }
+            }
+        }
+
+        private static int GetSellInVelocity(string itemName)
+        {
+            switch (itemName)
+            {
+                case Constants.Sulfuras:
+                    {
+                        return 0;
+                    }
+                default:
+                    {
+                        return -1;
+                    }
+            }
+        }
+
+        private static int GetQualityVelocity(Item item)
+        {
+            switch (item.Name)
+            {
+                case Constants.Sulfuras:
+                    {
+                        return 0;
+                    }
+                case Constants.AgedBrie:
+                    {
+                        return 1;
+                    }
+                case Constants.ConjuredManaCake:
+                    {
+                        return -2;
+                    }
+                case Constants.BackstagePass:
+                    {
+                        if (item.SellIn < 0)
+                        {
+                            return -item.Quality;
+                        }
+                        if (item.SellIn <= 5)
+                        {
+                            return 3;
+                        }
+                        if (item.SellIn <= 10)
+                        {
+                            return +2;
+                        }
+                        return +1;
+                    }
+                default:
+                    {
+                        if (item.SellIn < 0)
+                        {
+                            return -2;
+                        }
+                        return -1;
+                    }
+            }
+        }
+    }
+}
